c06d081089a41174a596afd242d4ddb103bc5e3d
cherryz21 <charansri21@gmail.com>
2020-07-23 11:28:02 -0400

    springboot restapi

---
+++ src/main/java/com/hcl/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.hcl.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		// TODO Auto-generated constructor stub
+		this.message =  message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
